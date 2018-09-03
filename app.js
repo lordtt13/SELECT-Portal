f4c8d1f79dfca4ca9c2f3d87610259e06bf4a1c1
Tanmay Thakur <thakurtanmay72@yahoo.com>
2018-09-03 18:44:44 +0530

    DB Added

--- app.js
+++ app.js
@@ -10,15 +10,18 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(expressSanitizer());
 app.set("view engine","ejs");
 app.use(express.static('public'));
-mongoose.connect("mongodb://localhost/school");
+mongoose.connect("mongodb://localhost/SELECT");
 app.use(methodOverride("_method"));
 
-var sSchema= new mongoose.Schema({name:String,password:String});
+var sSchema= new mongoose.Schema({
+    empid:Number,name:String,dept:String,deg:String,mob:String,email:String,cabin:String
+});
 
-var school= mongoose.model("school",sSchema);
+var school= mongoose.model("teacher",sSchema);
 
 app.get('/', function (req, res) {
-    var start=[{usecase:"Site Under Construction",image:"https://images.static-collegedunia.com/public/college_data/images/pdfthumb/1472706286PAT-brochure/full-0.jpg"}]
+    var start=[{usecase:"Site Under Construction",
+        image:"https://images.static-collegedunia.com/public/college_data/images/pdfthumb/1472706286PAT-brochure/full-0.jpg"}]
     res.render("landing.ejs",{start:start});
 });
 
