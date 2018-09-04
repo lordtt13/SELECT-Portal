d070412f26bef6698a82f1e330f889ed2911f60b
Anshuman Chhapolia <achhap.10.01@gmail.com>
2018-09-05 00:06:18 +0530

    Faculty Page Added
    Faculty Page was Added and Database Removed from Repository..(UX for faculty page still pending)

--- app.js
+++ app.js
@@ -13,11 +13,11 @@ app.use(express.static('public'));
 mongoose.connect("mongodb://localhost/SELECT");
 app.use(methodOverride("_method"));
 
-var sSchema= new mongoose.Schema({
+var teacherSchema= new mongoose.Schema({
     empid:Number,name:String,dept:String,deg:String,mob:String,email:String,cabin:String
 });
 
-var school= mongoose.model("teacher",sSchema);
+var teacher= mongoose.model("teacher",teacherSchema);
 
 app.get('/', function (req, res) {
     var start=[{usecase:"Site Under Construction",
@@ -25,6 +25,32 @@ app.get('/', function (req, res) {
     res.render("landing.ejs",{start:start});
 });
 
+app.get('/faculty',function(req,res){
+    teacher.find({}, function(err, teachers){
+        if(err){
+            console.log(err);
+        }
+        else{
+            var electrical=[],pedrive=[],instruments=[],controlauto=[];
+            teachers.forEach(function(t){
+                if(t.dept=="Electrical Engineering")
+                    electrical.push(t);
+                if(t.dept=="Instrumentation")
+                    instruments.push(t);
+                if(t.dept=="Energy and Power Electronics")
+                    pedrive.push(t);
+                if(t.dept=="Control and Automation")
+                    controlauto.push(t);
+                
+            });
+            
+                      console.log(controlauto);       
+            res.render("faculty.ejs",{electrical:electrical, instruments:instruments,pedrive:pedrive,controlauto:controlauto});
+            
+            }
+    });
+    
+});
 
 app.listen(63342,process.env.IP,function(){
     console.log("The Server has Started");
--- public/scripts/header.js
+++ public/scripts/header.js
@@ -1,14 +1,14 @@
 var navbarExpanded=false;
 $(document).ready(function(){
     navbarExpanded=false;
-    if(window.innerWidth>1024){
+    if(window.innerWidth>=1024){
         $('#items1').show();
     }
     else{
         $('#items1').hide();
     }
     $(window).resize(function(){
-        if(window.innerWidth>1024){
+        if(window.innerWidth>=1024){
             $('#items1').show();
             navbarExpanded=false;
         }
@@ -24,10 +24,36 @@ $(document).ready(function(){
         $('.nav-icon').toggleClass('icon-change');
         $('#items1').slideToggle();
         navbarExpanded=!navbarExpanded;
-        if(navbarExpanded)
-            window.setTimeout($('.navbar-selected').css('opacity', '0'),10000);
-        else
-            $('.navbar-selected').css('opacity', '1');
-
     });
+});
+document.addEventListener('DOMContentLoaded', function() {
+
+  var mn = $('.navbarC'),
+  core = $('.content-body').eq(0),
+  mns = 'page-header-scrolled',
+  bit, hdr;
+
+  $(window).resize(function() {
+if(window.innerWidth>=1024){
+    bit = 103;
+}
+      else{
+          bit=99;
+      }
+    hdr = $('.heading-bar').outerHeight();
+  })
+  .resize().scroll(function() {
+
+    if ($(this).scrollTop() >= hdr) {
+      mn.addClass(mns);
+      core.css('padding',bit+"px 0 30px 0");
+    } else {
+      mn.removeClass(mns);
+      core.attr('style', 'padding: 60px 0 30px 0;');
+    }
+  })
+  .on('load', function() {
+
+    $(this).scroll();
+  });
 });
