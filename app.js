34130408861584f80a0e01e924a7fa35545b09e3
Anshuman Chhapolia <achhap.10.01@gmail.com>
2018-09-06 02:12:28 +0530

    Faculty Page Sorting Done

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect("mongodb://localhost/SELECT");
 app.use(methodOverride("_method"));
 
 var teacherSchema= new mongoose.Schema({
-    empid:Number,name:String,dept:String,deg:String,mob:String,email:String,cabin:String
+    empid:Number,name:String,dept:String,deg:String,mob:String,email:String,cabin:String,rank:Number
 });
 
 var teacher= mongoose.model("teacher",teacherSchema);
@@ -41,8 +41,10 @@ app.get('/faculty',function(req,res){
                     controlauto.push(t);
                 
             });
-            
-                      console.log(controlauto);       
+            sortTeachers(electrical);
+            sortTeachers(pedrive);
+            sortTeachers(instruments);
+            sortTeachers(controlauto);
             res.render("faculty.ejs",{electrical:electrical, instruments:instruments,pedrive:pedrive,controlauto:controlauto});
             
             }
@@ -53,3 +55,27 @@ app.get('/faculty',function(req,res){
 app.listen(63342,process.env.IP,function(){
     console.log("The Server has Started");
 });
+
+
+function sortTeachers(teachers){
+    var l= teachers.length;
+    console.log(teachers);
+    var i,j;
+    for(i=l-1;i>0;i--){
+        for(j=0;j<i;j++){
+            if(teachers[j].rank>teachers[j+1].rank){
+                var temp= teachers[j];
+                teachers[j]=teachers[j+1];
+                teachers[j+1]=temp;
+            }
+            else if(teachers[j].rank==teachers[j+1].rank){
+                if(teachers[j].name>teachers[j+1].name){
+                var temp= teachers[j];
+                teachers[j]=teachers[j+1];
+                teachers[j+1]=temp;
+            }
+            }
+        }
+    }
+    
+}
